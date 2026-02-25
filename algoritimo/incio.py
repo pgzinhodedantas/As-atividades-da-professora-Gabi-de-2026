76bc3dd801173315ab91233e48563c50708ded26
pgzinhodedantas <gringohlegal@gmail.com>
2026-02-25 14:40:06 -0300

    Add files via upload

---
+++ algoritimo/incio.py
@@ -0,0 +1,10 @@
+nome ="ana"
+idade = 17
+altura = 1.65
+matriculado = True
+
+print(nome)
+print(idade)
+print(altura)
+print(matriculado)
+
---
+++ algoritimo/variaveis.py
@@ -0,0 +1,9 @@
+
+nome = "gaby"
+var1 = 10
+Var2 = 20
+
+print(nome)
+print(var1)
+print(var2)
+
