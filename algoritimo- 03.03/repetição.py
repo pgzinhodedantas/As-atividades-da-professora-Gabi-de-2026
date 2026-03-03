800afd7d45359927b9988bc1943b15efa09f0cf0
pgzinhodedantas <gringohlegal@gmail.com>
2026-03-03 14:26:29 -0300

    Add files via upload

---
+++ algoritimo- 03.03/repetição.py
@@ -0,0 +1,14 @@
+print("Ola, mundo!")
+print("Ola, mundo!")
+print("Ola, mundo!")
+print("Ola, mundo!")
+
+cont = 1
+
+while cont <= 10:
+    print("Ola, mundo!")
+    cont += 1 # cont = cont + 1
+print("FIM")
+
+
+print(list(range(10)))
