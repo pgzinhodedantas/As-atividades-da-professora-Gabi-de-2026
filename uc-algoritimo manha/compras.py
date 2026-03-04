5588a3ea3f69e2c10f86429a18089c48f550820c
pgzinhodedantas <gringohlegal@gmail.com>
2026-03-04 11:14:51 -0300

    Add files via upload

---
+++ uc-algoritimo manha/compras.py
@@ -0,0 +1,10 @@
+compras = ["arroz", "feijão", "leite", "pão", "ovos"]
+
+print("lista de compras atual:")
+print(compras)
+
+novo_item = input("digite um novo item para adicionar à lista: ")
+
+compras.append(novo_item)
+print("lista de compras atualizada:")
+print(compras)
---
+++ uc-algoritimo manha/listas.py
@@ -0,0 +1,49 @@
+nome1 = "caio"
+nome2 = "fabricio"
+nome3 = "paulo"
+nome4 = "lucas"
+
+nomes = ["caio", "fabricio", "paulo", "lucas"]
+print(nomes)
+
+dados = ["carlos", 0,1.70, True]
+print(dados)
+print(type(dados))
+print(type(dados[0]))
+print(type(dados[1]))
+print(type(dados[2]))
+print(type(dados[3]))
+
+adicionar = input()
+
+lista = ["cachorro","gato"]
+print("original;", lista)
+lista.append("coelho")
+print("atualizado: ", lista)
+
+lista.insert(1, "grilo")
+print("atualizado: ", lista)
+
+lista.extend(["macaco", "ovelha"])
+print("lista fianl ", lista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
