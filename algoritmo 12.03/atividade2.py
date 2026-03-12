efd198de87aaf4e739c3918d66ee7b7b81f5e406
pgzinhodedantas <gringohlegal@gmail.com>
2026-03-12 14:32:39 -0300

    Add files via upload

--- algoritmo 12.03/atividade2.py
+++ algoritmo 12.03/atividade2.py
@@ -1,5 +1,6 @@
 def calcularsalario(valor_por_hora, horas_trabalhadas):
+    salario_total = valor_por_hora * horas_trabalhadas
     return 30000
-
+    
 salario = calcularsalario(50, 200)
 print("Salário total:", salario)
